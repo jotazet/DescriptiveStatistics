00d1f1fe7588b41d9aa560ea57b9d1fd733c40a3
OzzyTheHuman <151674182+OzzyTheHuman@users.noreply.github.com>
2024-12-27 11:38:11 +0100

    Create ArithemticMean method

--- MeasuresOfLocation/Calculations/Calculations.cs
+++ MeasuresOfLocation/Calculations/Calculations.cs
@@ -8,6 +8,26 @@ namespace MeasuresOfLocation.Calculations
 {
     class Calculations
     {
+        public double[] Xi { get; init; }
 
+        public Calculations(double[] xi)
+        {
+            Xi = xi;
+        }
+
+        public static double ArithmeticMean(double[] Xi)
+        {
+            if (Xi == null || Xi.Length == 0)
+            {
+                throw new InvalidOperationException("Xi column is empty or not initialized.");
+            }
+                
+            double sum = 0;
+            foreach (var value in Xi)
+            {
+                sum += value;
+            }
+            return sum / Xi.Length;
+        }
     }
 }
