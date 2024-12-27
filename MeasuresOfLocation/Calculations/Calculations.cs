c1356b04b7f7c40459a9353470d3bece627d8152
OzzyTheHuman <151674182+OzzyTheHuman@users.noreply.github.com>
2024-12-27 12:31:36 +0100

    Create DiscreteWeightedArithemticMean method

--- MeasuresOfLocation/Calculations/Calculations.cs
+++ MeasuresOfLocation/Calculations/Calculations.cs
@@ -9,10 +9,16 @@ namespace MeasuresOfLocation.Calculations
     class Calculations
     {
         public double[] Xi { get; init; }
+        public double[] Fi { get; init; }
 
         public Calculations(double[] xi)
         {
             Xi = xi;
+        }
+        public Calculations(double[] xi, double[] fi)
+        {
+            Xi = xi;
+            Fi = fi;
         }
 
         public static double ArithmeticMean(double[] Xi)
@@ -28,6 +34,40 @@ namespace MeasuresOfLocation.Calculations
                 sum += value;
             }
             return sum / Xi.Length;
+        }
+
+        public static double DiscreteWeightedArithmeticMean(double[] Xi, double[] Fi)
+        {
+            if (Xi == null || Xi.Length == 0)
+            {
+                throw new InvalidOperationException("Xi column is empty or not initialized.");
+            }
+            if (Fi == null || Fi.Length == 0)
+            {
+                throw new InvalidOperationException("Fi column is empty or not initialized.");
+            }
+            if (Fi.Length != Xi.Length)
+            {
+                throw new InvalidOperationException("Not enough data in Xi or Fi column.");
+            }
+
+            double FiSumXiSum = 0;
+            double FiSum = 0;
+
+            for (int i = 0; i < Xi.Length; i++)
+            {
+                FiSumXiSum += Xi[i] * Fi[i];
+                FiSum += Fi[i];
+            }
+
+            if (FiSum != 0)
+            {
+                return FiSumXiSum / FiSum;
+            }
+            else
+            {
+                throw new InvalidOperationException("Sum of Fi cannot be zero.");
+            }
         }
     }
 }
